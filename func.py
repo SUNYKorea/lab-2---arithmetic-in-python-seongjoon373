e456060592a5448cab2c28dcdc8fb38b304688b2
SeongJoonKim <andrew42107@gmail.com>
2023-03-15 14:59:23 +0900

    SeongJoonKim lab2 classwork done

--- func.py
+++ func.py
@@ -1,15 +1,25 @@
 # -------------------------------------- Task 1 -----------------------------------
 def add(x, y):
     return x + y
+def sub(x, y):
+    return x - y
+def div(x, y):
+    return x / y
+def mult(x, y, z):
+    return x * y *z
+def exp(x, y):
+    return x ** y
 
 # TODO: Add definitions of sub(), div(), mult(), exp(), as well as neg() and sqrt().
 #       neg() should return the negation of the given number, and sqrt() should
 #       return the square root of the given number. 
 
 def neg(x):
+    return x * -1
     ... # fill here
 
 def sqrt(x):
+    return  x ** (1/2)
     ... # fill here
 
 # -------------------------------------- Task 2 -----------------------------------
@@ -17,13 +27,18 @@ def sqrt(x):
 # TODO: Implement the quadratic formula using *only* the functions defined here.
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
+
 a = 1
 b = -3
 c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+x1 = div(add(neg(b),sqrt(sub(exp(b,2),mult(4,a,c)))), mult(2,a,1))# TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b),sqrt(sub(exp(b,2),mult(4,a,c)))), mult(2,a,1)) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
 print("Second root:" + str(x2))
+
+
+
+
